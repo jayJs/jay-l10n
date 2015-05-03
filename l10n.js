42d0d4b1061795f8fc2c857d5c854c129d1f5ea1
Martin Sookael <m@451.ee>
2015-05-03 04:50:26 +0300

    added error reporting to l()

--- l10n.js
+++ l10n.js
@@ -2,18 +2,19 @@ function translate(language) {
   language = language || "eng";
   $("[data-translate]").each(function() {
     var word = $(this).attr("data-translate");
-    if( l10n && l10n[word] && l10n[word][language] ) { var translated = l10n[word][language]; }
-    else { var translated = ""; }
+    if( l10n && l10n[word] && l10n[word][language] ) {
+      var translated = l10n[word][language];
+    } else { var translated = ""; }
     $(this).empty().append(translated);
   });
 }
-
 
 function l(word) {
   if( language && l10n && l10n[word] && l10n[word][language] ) {
     var translated = l10n[word][language];
   } else {
     var translated = "";
+    console.error("l10n: '"+ word + "' not translated.")
   }
   return translated;
 }
