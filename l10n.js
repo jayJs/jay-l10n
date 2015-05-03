b93a6393d5e4faf17f0f0ce7e4f6a4e779b01dda
Martin Sookael <m@451.ee>
2015-05-03 04:24:58 +0300

    removed comments

--- l10n.js
+++ l10n.js
@@ -1,16 +1,16 @@
 function translate(language) {
-  language = language || "eng"; // if not language is set, set it to english
-  $(".l10n").each(function() { // go through all HTML elemtns with class "l10n"
-    var word = $(this).attr("data-word"); // get the word
+  language = language || "eng";
+  $(".l10n").each(function() {
+    var word = $(this).attr("data-word");
     if( l10n && l10n[word] && l10n[word][language] ) { var translated = l10n[word][language]; }
-    else { var translated = ""; } // if it's present in the opject, translate it, else leave blank.
-    $(this).empty().append(translated); // Empty the element and add the translation.
+    else { var translated = ""; }
+    $(this).empty().append(translated);
   });
 }
 
 function l(word, lang) {
-  lang = lang || "eng";  // if not language is set, set it to english
+  lang = lang || "eng";
   if( l10n && l10n[word] && l10n[word][lang] ) { var translated = l10n[word][lang]; }
-  else { var translated = ""; } // if it's present in the opject, translate it, else leave blank.
-  return translated; // return the translation
+  else { var translated = ""; }
+  return translated; 
 }
